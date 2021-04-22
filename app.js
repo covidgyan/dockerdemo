d096069da8f5a26802a3c6052af40c3c9e0a6de1
VIVEK ANAND <anandvivek2106@gmail.com>
2021-04-22 18:29:26 +0530

    1st com

--- app.js
+++ app.js
@@ -1,20 +1,76 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+//database hai
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit : 100, //important
+  host     : 'localhost',
+  user     : 'admin',
+  password : 'admin',
+  database : 'node_js_test',
+  debug    :  false
+});
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'html');
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// Make our db accessible to our router
+app.use(function(req,res,next) {
+    req.pool = pool;
+    next();
+});
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 
 module.exports = app;
